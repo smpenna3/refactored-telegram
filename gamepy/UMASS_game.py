c62a1f3b7d6c88af5ff18a23e0fea0e3822b382a
devendrashivraj <devendrashivraj@gmail.com>
2018-10-13 18:50:06 -0400

    oop apple

--- gamepy/UMASS_game.py
+++ gamepy/UMASS_game.py
@@ -1,62 +1,45 @@
 import pygame
 from pygame.locals import *
 
+SCREENSIZE = 300
+
+class Apple(pygame.sprite.Sprite):
+
+    def __init__(self, x, y):
+        pygame.sprite.Sprite.__init__(self, pygame.sprite.RenderUpdates())
+        self.x = x
+        self.y = y
+
+        self.image = pygame.image.load("apple.png")
+
+    def drawScreen(self, screen):
+        screen.blit(self.image, (self.x, self.y))
 
 def main():
     pygame.init()
-    
-    image = pygame.image.load("apple.png")
-    pygame.display.set_caption("apple")
-    width = image.get_width()
-    height = image.get_height()
-    SIZE = 300
+    screen = pygame.display.set_caption("apple")
+    screen = pygame.display.set_mode((SCREENSIZE, SCREENSIZE))
 
-    imageX = (SIZE - width)/2
-    imageY = (SIZE - height)/2
-    angle = 0
-    
-    screen = pygame.display.set_mode((SIZE,SIZE))
-    screen.blit(image,(imageX, imageY))
-    pygame.display.flip() 
+    apple = Apple(100, 100)
 
+
+    clock = pygame.time.Clock()
+    
     while True:
         for event in pygame.event.get():
             if event.type == QUIT:
                 return 0
 
-            keystate = pygame.key.get_pressed()
+        keystate = pygame.key.get_pressed()
 
-            if keystate[K_RIGHT]:
-                imageX += 10
-            elif keystate[K_LEFT]:
-                imageX -= 10
-            elif keystate[K_UP]:
-                imageY -= 10
-            elif keystate[K_DOWN]:
-                imageY += 10
-            elif keystate[K_r]:
-                angle += 10
-            elif keystate[K_l]:
-                angle -= 10
 
-            screen.fill((0,0,0))
+        apple.drawScreen(screen)
+        pygame.display.flip()
 
-            originalCenter = image.get_rect().center
-            rotatedImage = pygame.transform.rotate(image, angle)
-            rotatedImage.get_rect().center = originalCenter
+        clock.tick(40)
 
-            if imageX < 0:
-                imageX = 0
-            if imageX > SIZE - width:
-                imageX = SIZE - width
-            if imageY < 0:
-                imageY = 0   
-            if imageY > SIZE - height:
-                imageY = SIZE - height
+    pygame.quit()
 
-            screen.blit(rotatedImage,(imageX, imageY))
-                
-            pygame.display.flip()
-                
+
 if __name__=="__main__":
     main()
